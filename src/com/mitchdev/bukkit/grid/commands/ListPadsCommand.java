d123b46d958183285cf4b6f8a79d5d1063899763
Mitchell Clode <mitchell@mitchdev.com>
2012-07-15 11:44:28 +0930

    '/grid list' now shows networks that the user has permission to see.

--- src/com/mitchdev/bukkit/grid/commands/ListPadsCommand.java
+++ src/com/mitchdev/bukkit/grid/commands/ListPadsCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 
 import com.mitchdev.bukkit.grid.CommandHandler;
 import com.mitchdev.bukkit.grid.Grid;
+import com.mitchdev.bukkit.grid.Network;
 import com.mitchdev.bukkit.grid.Pad;
 import com.mitchdev.bukkit.grid.Visibility;
 
@@ -19,62 +20,29 @@ public class ListPadsCommand extends CommandHandler {
 	@Override
 	public boolean onCommand ( Grid grid, CommandSender sender, Command cmd, String commandLabel, String[] args ) {
 		
+		if ( !sender.hasPermission( "grid.list" ) ) {
+			sender.sendMessage(Grid.getChatPrefix() + "ERROR: You do not have permission to use this command..");
+			return true;
+		}
+		
 		if ( cmd.getName().equalsIgnoreCase("grid") ) {
 				
 			if ( commandLabel.equalsIgnoreCase("gl") || ( args.length > 0 && args[0].equalsIgnoreCase( "list" ) ) ) {
-				
-				boolean seeHidden = false;
-				boolean seeUnlisted = false;
-				boolean isServer = false;
-				
-				if ( sender instanceof Player ) {
-					
-					Player p = (Player)sender;
-					if ( p.hasPermission("grid.list.hidden") ) {
-						seeHidden = true;
-					}
-					if( p.hasPermission("grid.list.unlisted") ) {
-						seeUnlisted = true;
-					}
-					
-				}
-				else {
-					seeHidden = true;
-					seeUnlisted = true;
-					isServer = true;
-				}
 				
 				sender.sendMessage( ChatColor.AQUA + "--- Pads ---");
 				
 				// Iterate over the default pads (IE: Don't belong to a network)
 				
-				sender.sendMessage ( ChatColor.YELLOW + "default:" );
+				listNetwork ( sender, grid.getGlobalNetwork() );
 				
-				for ( Pad pad : grid.getPads() ) {
-					if ( pad.getVisibility() == Visibility.Hidden && !seeHidden ) {
-						continue;
-					}
-					if( pad.getVisibility() == Visibility.Unlisted && !seeUnlisted ) {
-						continue;
-					}
+				for ( Network network : grid.getNetworks() ) {
 					
-					String vis = "-";
+					if ( sender.hasPermission("grid.list.network." + network.getId())) {
 					
-					switch ( pad.getVisibility() ) {
-					
-					case Visible:
-						vis = ChatColor.GREEN + "V" + ChatColor.RESET;
-						break;
-					case Hidden:
-						vis = ChatColor.YELLOW + "H" + ChatColor.RESET;
-						break;
-					case Unlisted:
-						vis = ChatColor.RED + "U" + ChatColor.RESET;
-						break;
-					
+						listNetwork ( sender, network );
+						
 					}
 					
-					sender.sendMessage( String.format(" %s - %s %s %s", pad.getName(), vis, pad.getSwitches().size(), pad.getTriggers().size() ) );
 				}
 				
 				return true;
@@ -85,6 +53,59 @@ public class ListPadsCommand extends CommandHandler {
 		
 		return false;
 		
+	}
+	
+	private void listNetwork ( CommandSender sender, Network network ) {
+		
+		boolean seeHidden = false;
+		boolean seeUnlisted = false;
+		boolean isServer = false;
+		
+		if ( sender instanceof Player ) {
+			
+			Player p = (Player)sender;
+			if ( p.hasPermission("grid.list.hidden") ) {
+				seeHidden = true;
+			}
+			if( p.hasPermission("grid.list.unlisted") ) {
+				seeUnlisted = true;
+			}
+			
+		}
+		else {
+			seeHidden = true;
+			seeUnlisted = true;
+			isServer = true;
+		}
+		
+		sender.sendMessage ( ChatColor.YELLOW + network.getId() + ":" );
+		
+		for ( Pad pad : network.getPads() ) {
+			if ( pad.getVisibility() == Visibility.Hidden && !seeHidden ) {
+				continue;
+			}
+			if( pad.getVisibility() == Visibility.Unlisted && !seeUnlisted ) {
+				continue;
+			}
+			
+			String vis = "-";
+			
+			switch ( pad.getVisibility() ) {
+			
+			case Visible:
+				vis = ChatColor.GREEN + "V" + ChatColor.RESET;
+				break;
+			case Hidden:
+				vis = ChatColor.YELLOW + "H" + ChatColor.RESET;
+				break;
+			case Unlisted:
+				vis = ChatColor.RED + "U" + ChatColor.RESET;
+				break;
+			
+			}
+			
+			sender.sendMessage( String.format(" %s - %s %s %s", pad.getName(), vis, pad.getSwitches().size(), pad.getTriggers().size() ) );
+		}
 	}
 	
 }
